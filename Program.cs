0bce93280f785798afcbdec9a420efca045ef330
1451010148quang@ou.edu.vn <1451010148quang@ou.edu.vn>
2018-01-25 08:16:26 +0700

    thay doi file program

--- Program.cs
+++ Program.cs
@@ -16,6 +16,7 @@ namespace Demo
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+//quang dep trai
         }
     }
 }
